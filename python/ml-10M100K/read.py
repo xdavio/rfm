edb7995c1e521ff2687a8af720329654214ec383
xdavio <xdavio@gmail.com>
2017-02-16 23:48:40 -0600

    ml read and fit

--- python/ml-10M100K/read.py
+++ python/ml-10M100K/read.py
@@ -19,3 +19,36 @@ with open(RATINGS, 'r') as f:
     for line in f.readlines():
         tags.append(line.strip().split('::'))
 
+df_movies = pd.DataFrame(movies)
+df_movies.columns = ['movie_id', 'title', 'genres']
+df_ratings = pd.DataFrame(ratings)
+df_ratings.columns = ['user_id', 'movie_id', 'rating', 'timestamp']
+df_tags = pd.DataFrame(tags)
+df_tags.columns = ['user_id', 'movie_id', 'tag', 'timestamp']
+
+unique_users = df_ratings.user_id.unique()
+users_id2code = {i:code for i,code in
+                 enumerate(unique_users)},
+users_code2id =  {code:i for i,code in
+                  enumerate(unique_users)}
+
+unique_movies = df_ratings.movie_id.unique()
+movies_id2code = {i:code for i,code in
+                  enumerate(unique_movies)},
+movies_code2id =  {code:i for i,code in
+                  enumerate(unique_movies)}
+
+nrow = df_ratings.shape[0]
+
+y = df_ratings.rating
+y_ind = np.arange(nrow)
+
+row = np.concatenate((y_ind, y_ind))
+
+users = df_ratings['user_id'].apply(lambda code: users_code2id[code]).values
+movies = df_ratings['movie_id'].apply(lambda code: movies_code2id[code]).values + \
+         unique_uesrs.shape[0]
+
+col = np.concatenate((
+    users, movies
+))
