26dd857babbf065411c450737c528200fa1fe51c
jdavis@civisanalytics.com <jdavis@civisanalytics.com>
2017-03-10 18:42:17 -0600

    refactored tests

--- python/pyfm/tests/test_fm_sgd.py
+++ python/pyfm/tests/test_fm_sgd.py
@@ -1,3 +1,4 @@
+from copy import copy
 import numpy as np
 import pytest
 
@@ -18,8 +19,8 @@ def setup(nrow=5000, ncol=10, seed=23098472):
     cols = np.concatenate((
         np.repeat(0, NUM_VAL/2), np.repeat(1, NUM_VAL/2)
     ))
-    y = np.random.normal(0, 1, nrow) / 10 + 2 * values[:(NUM_VAL/2)] - \
-        1 * values[(NUM_VAL/2):NUM_VAL]
+    y = np.random.normal(0, 1, nrow) / 10 + 2 * values[:int(NUM_VAL/2)] - \
+        1 * values[int(NUM_VAL/2):NUM_VAL]
 
     y_ind = np.arange(nrow)
     K = 3
@@ -30,13 +31,30 @@ def setup(nrow=5000, ncol=10, seed=23098472):
 def params1():
     opt_params_sgd = {
         "optimizer": "sgd",
-        "minibatch": 100,
+        "minibatch": 128,
         "n_outer": 10000,
         "eta" : 0.1,
         "lambda" : 0,
         "eps": 1e-8
     }
     return opt_params_sgd
+
+
+@pytest.fixture()
+def params_adam(params1):
+    opt_params = params1
+    opt_params = opt_params.copy()
+    opt_params['optimizer'] = 'adam'
+    opt_params['eta'] = .001
+    return opt_params
+
+
+@pytest.fixture()
+def params_adagrad(params1):
+    opt_params = params1
+    opt_params = opt_params.copy()
+    opt_params['optimizer'] = 'adagrad'
+    return opt_params
 
 
 def test_sgd_main_effects(setup, params1, seed=238423):
@@ -49,13 +67,57 @@ def test_sgd_main_effects(setup, params1, seed=238423):
     print(fm.f_beta0)
     print(fm.f_beta)
     print(fm.f_v)
-    np.testing.assert_almost_equal(fm.f_beta0, 0, 1)
-    np.testing.assert_almost_equal(fm.f_beta[0], 2, 1)
-    np.testing.assert_almost_equal(fm.f_beta[1], -1, 1)
+    np.testing.assert_almost_equal(fm.f_beta0, 0, 2)
+    np.testing.assert_almost_equal(fm.f_beta[0], 2, 2)
+    np.testing.assert_almost_equal(fm.f_beta[1], -1, 2)
 
     # assert that too few iterations produces high in-sample loss
     loss1 = fm.loss(values, rows, cols, y)
     params1['n_outer'] = 10
+    fm.fit(values, rows, cols, y)
+    loss2 = fm.loss(values, rows, cols, y)
+    assert loss1 < loss2
+
+
+def test_adam_main_effects(setup, params_adam, seed=238423):
+    np.random.seed(seed)    
+    para = params_adam    
+    values, rows, cols, y, y_ind, K = setup
+    
+    fm = FM(K, para)
+    fm.fit(values, rows, cols, y)
+    print(fm.f_beta0)
+    print(fm.f_beta)
+    print(fm.f_v)
+    np.testing.assert_almost_equal(fm.f_beta0, 0, 2)
+    np.testing.assert_almost_equal(fm.f_beta[0], 2, 2)
+    np.testing.assert_almost_equal(fm.f_beta[1], -1, 2)
+
+    # assert that too few iterations produces high in-sample loss
+    loss1 = fm.loss(values, rows, cols, y)
+    para['n_outer'] = 10
+    fm.fit(values, rows, cols, y)
+    loss2 = fm.loss(values, rows, cols, y)
+    assert loss1 < loss2
+
+
+def test_adagrad_main_effects(setup, params_adagrad, seed=238423):
+    np.random.seed(seed)    
+    para = params_adagrad    
+    values, rows, cols, y, y_ind, K = setup
+    
+    fm = FM(K, para)
+    fm.fit(values, rows, cols, y)
+    print(fm.f_beta0)
+    print(fm.f_beta)
+    print(fm.f_v)
+    np.testing.assert_almost_equal(fm.f_beta0, 0, 2)
+    np.testing.assert_almost_equal(fm.f_beta[0], 2, 2)
+    np.testing.assert_almost_equal(fm.f_beta[1], -1, 2)
+
+    # assert that too few iterations produces high in-sample loss
+    loss1 = fm.loss(values, rows, cols, y)
+    para['n_outer'] = 10
     fm.fit(values, rows, cols, y)
     loss2 = fm.loss(values, rows, cols, y)
     assert loss1 < loss2
--- python/pyfm/tests/test_fm_sgd_interaction.py
+++ python/pyfm/tests/test_fm_sgd_interaction.py
@@ -1,8 +1,12 @@
 from scipy.sparse import coo_matrix
 import numpy as np
 import pytest
+import logging
 
 from pyfm import FM
+
+logging.basicConfig(level=logging.DEBUG)
+
 
 @pytest.fixture()
 def data(nrow=5000, ncol=10, seed=4832):
@@ -20,37 +24,75 @@ def data(nrow=5000, ncol=10, seed=4832):
     X = coo_matrix(X)
     return X.data, X.row, X.col, y
 
+
 @pytest.fixture()
 def params1():
     opt_params_sgd = {
-        "optimizer": "adam",
+        "optimizer": "sgd",
         "minibatch": 100,
         "n_outer": 30000,
         "eta" : 0.001,
         "lambda" : 0,
         "eps": 1e-8
     }
-    K = 4
+    K = 10
     return K, opt_params_sgd
 
+
+@pytest.fixture()
+def params_adam(params1):
+    K, opt_params = params1
+    opt_params = opt_params.copy()
+    opt_params['optimizer'] = 'adam'
+    return K, opt_params.copy()
+
+
+@pytest.fixture()
+def params_adagrad(params1):
+    K, opt_params = params1
+    opt_params = opt_params.copy()
+    opt_params['optimizer'] = 'adagrad'
+    return K, opt_params.copy()
+
+
 def test_sgd_int(data, params1):
+    log = logging.getLogger('test_sgd_int')
     values, rows, cols, y = data
     K, opt_params = params1
     
-    fm = FM(K, opt_params)
-    y_mean = y.mean()
-    y_sd = y.std()
-    y = (y - y_mean) / y_sd
+    fm = FM(K, opt_params, standardize=True)
     fm.fit(values, rows, cols, y)
 
-    print(fm.f_beta0)
-    print(fm.f_beta[:3])
-    print(y_mean, y_sd)
-    print(fm.f_beta0 * y_sd + y_mean)
-    print(fm.f_beta[:3] * y_sd)
-    print(fm.f_v[1,:].dot(fm.f_v[2,:]) * y_sd)
-    #np.testing.assert_almost_equal(fm.f_beta0, 0, 1)
-    np.testing.assert_almost_equal(fm.f_beta[0], 1, 1)
-    np.testing.assert_almost_equal(fm.f_beta[1], 2, 1)
-    np.testing.assert_almost_equal(fm.f_beta[2], -1, 1)    
+    est_int12 = fm.f_v[1, :].dot(fm.f_v[2, :])
 
+    log.debug('intercept: {}'.format(fm.f_beta0))
+    log.debug('first 3 main effects: {}'.format(fm.f_beta[:3]))
+    log.debug('remaining main effects: {}'.format(fm.f_beta[3:10]))
+    log.debug('interaction effect of 1+2: {}'.format(est_int12))
+
+    np.testing.assert_almost_equal(fm.f_beta0, 0, 2)
+    np.testing.assert_almost_equal(fm.f_beta[0], 1, 2)
+    np.testing.assert_almost_equal(fm.f_beta[1], 2, 2)
+    np.testing.assert_almost_equal(fm.f_beta[2], -1, 2)
+
+
+def test_adam_int(data, params_adam):
+    log = logging.getLogger('test_sgd_int')
+    values, rows, cols, y = data
+    K, opt_params = params_adam
+    
+    fm = FM(K, opt_params, standardize=True)
+    fm.fit(values, rows, cols, y)
+
+    est_int12 = fm.f_v[1, :].dot(fm.f_v[2, :])
+
+    log.debug('intercept: {}'.format(fm.f_beta0))
+    log.debug('first 3 main effects: {}'.format(fm.f_beta[:3]))
+    log.debug('remaining main effects: {}'.format(fm.f_beta[3:10]))
+    log.debug('interaction effect of 1+2: {}'.format(est_int12))
+
+    np.testing.assert_almost_equal(fm.f_beta0, 0, 2)
+    np.testing.assert_almost_equal(fm.f_beta[0], 1, 2)
+    np.testing.assert_almost_equal(fm.f_beta[1], 2, 2)
+    np.testing.assert_almost_equal(fm.f_beta[2], -1, 2)
+    
